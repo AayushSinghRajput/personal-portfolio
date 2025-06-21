116486cc5d17a5f64485009f3ec039e2776ac5de
Aayush Kumar Singh <078bct003@ioepc.edu.np>
2025-06-21 12:28:51 +0545

    some changes

--- src/Apis/contact.ts
+++ src/Apis/contact.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { ContactData } from "../types";
 
-const BACKEND_URL = process.env.REACT_BACKEND_URL;
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 
 export const contactApi = async (contactData: ContactData) => {
   const response = await axios.post(
